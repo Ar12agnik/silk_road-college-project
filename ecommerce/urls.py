9a1e2cb6332121e803dfe96bd8dffbef047f66a6
Agnik Roy <102238499+Ar12agnik@users.noreply.github.com>
2024-12-09 20:09:28 +0530

    update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_view,name='home'),
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
-    path('logout', LogoutView.as_view(template_name='ecom/logout.html'),name='logout'),
+    path('logout', views.log_out,name='logout'),
     path('aboutus', views.aboutus_view),
     path('contactus', views.contactus_view,name='contactus'),
     path('search', views.search_view,name='search'),
